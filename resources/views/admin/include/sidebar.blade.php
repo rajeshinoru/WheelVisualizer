e0347df416ad50bd27e560dc10a4aedc6d4293e3
Nagarajan <nagarajanv@inoru.com>
2020-01-22 16:00:18 +0530

    Admin panel UI Changes

--- resources/views/admin/include/sidebar.blade.php
+++ resources/views/admin/include/sidebar.blade.php
@@ -8,7 +8,7 @@
             <div class="left-custom-menu-adp-wrap comment-scrollbar">
                 <nav class="sidebar-nav left-sidebar-menu-pro">
                     <ul class="metismenu" id="menu1">
-                        <li class="active">
+                        <li class="">
                             <a class="has-arrow" href="index.html">
                                    <span class="educate-icon educate-home icon-wrap"></span>
                                    <span class="mini-click-non">Home</span>
@@ -40,6 +40,8 @@
                         <li>
                             <a title="Landing Page" href="{{url('admin/setting')}}" aria-expanded="false"><span class="educate-icon educate-settings icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Settings</span></a>
                         </li>
+
+
 
                     </ul>
                 </nav>
