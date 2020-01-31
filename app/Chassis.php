a9677d4325df96b1e77cd5e2a49244c259178f9a
Rajesh S <rajesh.s@inoru.com>
2020-01-31 20:06:16 +0530

    Chassis Module Worked

---
+++ app/Chassis.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Chassis extends Model
+{
+    //
+}
---
+++ app/Http/Controllers/ChassisModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ChassisModel;
+use Illuminate\Http\Request;
+
+class ChassisModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ChassisModel  $chassisModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ChassisModel $chassisModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ChassisModel  $chassisModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChassisModel $chassisModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ChassisModel  $chassisModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ChassisModel $chassisModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ChassisModel  $chassisModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChassisModel $chassisModel)
+    {
+        //
+    }
+}
