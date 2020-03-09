478a72544f4cdb4fe54841e9c2d6ed20edb0a7be
Rajesh S <rajesh.s@inoru.com>
2020-03-09 21:02:33 +0530

    View Title Chanegs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Wheel</title>
+    <title>{{Setting::get('site_title','Wheel')}}</title>
     <link rel="stylesheet" href="{{ asset('css/ontheme/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/ontheme/animate.css') }}">
     <link rel="stylesheet" href="{{ asset('css/ontheme/category-feature.css') }}">
