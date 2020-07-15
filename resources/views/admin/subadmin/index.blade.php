877471819c04cb41f0b57b17cc06a63d00a86e11
Rajesh S <rajesh.s@inoru.com>
2020-07-15 23:58:05 +0530

    Worked On Subadmin panel

---
+++ resources/views/admin/subadmin/index.blade.php
@@ -0,0 +1,246 @@
+@extends('admin.layouts.app')
+
+@section('content')
+
+<div class="product-status mg-b-15">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="product-status-wrap drp-lst">
+                    <h4>Subadmin List</h4>
+                    <div class="add-product">
+                        <!-- <a data-toggle="modal" data-target="#myModal">Add User</a> -->
+                    </div>
+                    <div class="asset-inner">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>User ID</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Email</th>
+                                    <th>Created At</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            @forelse(@$users as $key => $user)
+                            <tr>
+                                <td>{{$key+1}}</td>
+                                <td>{{@$user->userid}}</td>
+                                <td>{{@$user->fname}}</td>
+                                <td>{{@$user->lname}}</td>
+                                <td>{{@$user->email}}</td>
+                                <td>{{@$user->created_at}}</td>
+                                <td>
+                                    <a type="button" class="btn btn-info" data-toggle="modal" data-target="#editModal{{$key}}"><i class="fa fa-edit"></i></a>
+
+
+                                    <a type="button" class="btn btn-danger delete-user" data-key="{{$key}}"><i class="fa fa-trash"></i></a>
+                                    <form id="delete-form-{{$key}}" action="{{route('admin.user.destroy',$user->id)}}" method="POST" novalidate="">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    </form>
+                                </td>
+                            </tr>
+                            <div class="modal fade" id="editModal{{$key}}" role="dialog">
+                                <div class="modal-dialog admin-form">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h4 class="modal-title">Edit User</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div id="dropzone1" class="pro-ad addcoursepro">
+                                                <form action="{{ route('admin.user.update', $user->id)}}" class=" needsclick addcourse" method="POST" id="update-post" enctype="multipart/form-data">
+                                                    {{csrf_field()}}
+                                                    {{method_field('PATCH')}}
+                                                    <div class="row">
+                                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                            <div class="col-md-2">
+                                                                <label>First Name</label>
+                                                            </div>
+                                                            <div class="col-md-10">
+                                                                <input type="text" name="fname" class="form-control" placeholder="Give the First Name" required="" value="{{$user->fname}}">
+                                                            </div>
+                                                        </div> 
+                                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                            <div class="col-md-2">
+                                                                <label>Last Name</label>
+                                                            </div>
+                                                            <div class="col-md-10">
+                                                                <input type="text" name="lname" class="form-control" placeholder="Give the Last Name" required="" value="{{$user->lname}}">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <br>
+
+                                                    <div class="row">
+                                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                            <div class="col-md-2">
+                                                                <label>Email</label>
+                                                            </div>
+                                                            <div class="col-md-10">
+                                                                <input type="email" name="email" class="form-control" placeholder="Give the email id" required="" value="{{$user->email}}">
+                                                            </div>
+                                                        </div> 
+                                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                            <div class="col-md-2">
+                                                                <label>Phone</label>
+                                                            </div>
+                                                            <div class="col-md-10">
+                                                                <input type="text" name="phone" class="form-control" placeholder="Give the phone number" value="{{$user->phone}}">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <br>
+                                                    <div class="row">
+                                                        <div class="col-lg-6">
+                                                            <div class="payment-adress">
+                                                                <button type="submit" class="btn btn-primary waves-effect waves-light">Submit</button>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-lg-6">
+                                                            <div class="payment-adress">
+                                                                <a class="btn btn-danger waves-effect waves-light" data-dismiss="modal">Cancel</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                            </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
+                                    </div>
+                                </div>
+                            </div>
+                            @empty
+                            <tr>
+                                <td colspan="5">No Users found</td>
+                            </tr>
+                            @endforelse
+
+                            <!-- <tfoot>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Created At</th>
+                                    <th>Setting</th> 
+                                </tr>
+                            </tfoot> -->
+                        </table>
+                    </div>
+                    {{@$users->links()}}
+                    <!-- <div class="custom-pagination">
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>
+                    </div> -->
+
+
+                    <!--  New Model Start-->
+                    <div class="modal fade" id="myModal" role="dialog">
+                        <div class="modal-dialog admin-form">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">User Information</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <!-- New Model Content Start -->
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        <div class="product-payment-inner-st">
+                                            <ul id="myTabedu1" class="tab-review-design">
+                                                <li class="active"><a href="#description2">Basic Details</a></li>
+                                            </ul>
+                                            <div id="myTabContent" class="tab-content custom-product-edit">
+                                                <div class="product-tab-list tab-pane fade active in" id="description2">
+                                                    <div class="row">
+                                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                            <div class="review-content-section">
+                                                                <div id="dropzone1" class="pro-ad">
+                                                                    <form action="{{url('/admin/user/')}}" class="dropzone dropzone-custom needsclick add-professors dz-clickable" id="demo1-upload" method="POST">
+                                                                        {{@csrf_field()}}
+                                                                        <div class="row">
+                                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                                                <div class="form-group">
+                                                                                    <label for="fname">First Name</label>
+                                                                                    <input name="fname" type="text" class="form-control" placeholder="First Name" value="" required="">
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                                                <div class="form-group">
+                                                                                    <label for="lname">Last Name</label>
+                                                                                    <input type="text" name="lname" class="form-control" placeholder="Last Name" value="" required="">
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="row">
+                                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                                                <div class="form-group">
+                                                                                    <label for="lname">Mobile</label>
+                                                                                    <input type="text" name="mobile" class="form-control" placeholder="(+91) Mobile Number " value="">
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                                                <div class="form-group">
+                                                                                    <label for="fname">Email</label>
+                                                                                    <input name="email" type="email" class="form-control" placeholder="Email " value="" required="">
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="row">
+                                                                            <div class="col-lg-12">
+                                                                                <div class="payment-adress">
+                                                                                    <input type="submit" class="btn btn-primary waves-effect waves-light" value="Submit">
+
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </form>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- New Model Content End -->
+                                </div>
+                                <div class="modal-footer">
+                                    <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--New Model End  -->
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div> 
+@endsection
+
+@section('custom_scripts')
+<script type="text/javascript">
+ 
+    $('.delete-user').click(function(){
+            if (confirm("Are you sure want to remove user?")) {
+                $('#delete-form-'+$(this).data('key')).submit();
+            }
+            return false;
+    })
+</script>
+@endsection
