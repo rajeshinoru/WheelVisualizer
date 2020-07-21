6efb0a162d055957d74b52d239a326a70e033631
Rajesh S <rajesh.s@inoru.com>
2020-07-21 20:50:40 +0530

    Worked on the Admin logs

--- resources/views/vendor/laravel-log-viewer/log.blade.php
+++ resources/views/vendor/laravel-log-viewer/log.blade.php
@@ -160,33 +160,31 @@ $is_write_access = VerifyAccess('logs','write');
                                         </thead>
                                         <tbody>
 
-                                            @foreach($logs as $key => $log)
-                                            @if($log['level'] == 'info')
-                                            <tr data-display="stack{{{$key}}}">
-                                                @if ($standardFormat)
-                                                <td class="nowrap text-{{{$log['level_class']}}}">
-                                                    <span class="fa fa-{{{$log['level_img']}}}" aria-hidden="true"></span>&nbsp;&nbsp;{{$log['level']}}
-                                                </td>
-                                                <td class="text">{{$log['context']}}</td>
-                                                @endif
-                                                <td class="date">{{{$log['date']}}}</td>
-                                                <td class="text">
-                                                    @if ($log['stack'])
-                                                    <button type="button" class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2" data-display="stack{{{$key}}}">
-                                                        <span class="fa fa-search"></span>
-                                                    </button>
-                                                    @endif
-                                                    {{{$log['text']}}}
-                                                    @if (isset($log['in_file']))
-                                                    <br />{{{$log['in_file']}}}
-                                                    @endif
-                                                    @if ($log['stack'])
-                                                    <div class="stack" id="stack{{{$key}}}" style="display: none; white-space: pre-wrap;">{{{ trim($log['stack']) }}}
-                                                    </div>
-                                                    @endif
-                                                </td>
-                                            </tr>
-                                            @endif
+                                            @foreach($logs as $key => $log) 
+                                              <tr data-display="stack{{{$key}}}">
+                                                  @if ($standardFormat)
+                                                  <td class="nowrap text-{{{$log['level_class']}}}">
+                                                      <span class="fa fa-{{{$log['level_img']}}}" aria-hidden="true"></span>&nbsp;&nbsp;{{$log['level']}}
+                                                  </td>
+                                                  <td class="text">{{$log['context']}}</td>
+                                                  @endif
+                                                  <td class="date">{{{$log['date']}}}</td>
+                                                  <td class="text">
+                                                      @if ($log['stack'])
+                                                      <button type="button" class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2" data-display="stack{{{$key}}}">
+                                                          <span class="fa fa-search"></span>
+                                                      </button>
+                                                      @endif
+                                                      {{{$log['text']}}}
+                                                      @if (isset($log['in_file']))
+                                                      <br />{{{$log['in_file']}}}
+                                                      @endif
+                                                      @if ($log['stack'])
+                                                      <div class="stack" id="stack{{{$key}}}" style="display: none; white-space: pre-wrap;">{{{ trim($log['stack']) }}}
+                                                      </div>
+                                                      @endif
+                                                  </td>
+                                              </tr> 
                                             @endforeach
 
                                         </tbody>
