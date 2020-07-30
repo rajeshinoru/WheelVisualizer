b1447b82f486c35d83e6c166c421781ff2cbce3c
Rajesh S <rajesh.s@inoru.com>
2020-07-30 20:17:29 +0530

    Issue for loading

--- app/Http/Controllers/WheelProductController.php
+++ app/Http/Controllers/WheelProductController.php
@@ -97,9 +97,9 @@ class WheelProductController extends Controller
  
             $products = WheelProduct::with('wheel')->select('id', 'prodbrand','detailtitle', 'prodmodel', 'prodfinish', 'prodimage', 'wheeldiameter', 'wheelwidth', 'prodtitle', 'price', 'partno','partno_old','wheeltype','rf_lc','boltpattern1','offset1','offset2','boltpattern1','wheeltype');
 
-            if($zipcode==null){
-                $products= $products->where('id','<=',10);
-            }
+            // if($zipcode==null){
+            //     $products= $products->where('id','<=',10);
+            // }
 
 
             $branddesc = [];
